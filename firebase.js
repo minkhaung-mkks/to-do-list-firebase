8da5ea0eda16d32dde4af9797822c06106b3b22b
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-21 18:56:52 +0630

    Create New to do on adding

--- firebase.js
+++ firebase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebas
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
 import { getDatabase, ref, push, set } from 'https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js'
 import { databaseURL } from "./secrets.js";
+import { CreateAgendaCard } from "./utlis.js";
 // import { firebaseConfig } from "./secrets.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,16 +20,19 @@ const database = getDatabase(app)
 
 const userAgenda = ref(database, "To-Dos")
 const inputField = document.getElementById('new_item_input')
+const agendaBox = document.querySelector('.agenda_box')
 
-const addNewToDo = () => {
+const AddNewToDo = () => {
     let inputValue = inputField.value
     push(userAgenda, inputValue)
+    let newCard = CreateAgendaCard(inputValue)
+    agendaBox.append(newCard)
     console.log(`${inputValue} added to the database`)
 }
 
 inputField.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        addNewToDo()
+        AddNewToDo()
     }
 })
 
