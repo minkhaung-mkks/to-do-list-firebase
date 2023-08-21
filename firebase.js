2fb066212b877d4610cede83aeb0197c977170b5
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-21 18:43:19 +0630

    Removed Console logs and fixed Typos

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
 import { getDatabase, ref, push, set } from 'https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js'
-import { databaseURL } from "./secrets";
+import { databaseURL } from "./secrets.js";
 // import { firebaseConfig } from "./secrets.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -12,14 +12,10 @@ import { databaseURL } from "./secrets";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     databaseURL: databaseURL,
-
 }
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app)
-
-console.log(app)
-console.log(database)
 
 const userAgenda = ref(database, "To-Dos")
 const inputField = document.getElementById('new_item_input')
