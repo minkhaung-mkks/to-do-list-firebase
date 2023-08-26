417ef783276d8377674c35a611cd4eb5f32548ae
Min Khaung <minkhaung.mkks@gmail.com>
2023-08-26 22:09:46 +0630

    Bug Fix

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-analytics.js";
 import { getDatabase, ref, push, set, onValue, remove } from 'https://www.gstatic.com/firebasejs/10.2.0/firebase-database.js'
-import { databaseURL } from "./secrets.js";
 import { CreateAgendaCard } from "./utlis.js";
 // import { firebaseConfig } from "./secrets.js";
 // TODO: Add SDKs for Firebase products that you want to use
